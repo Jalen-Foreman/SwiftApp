37dff47d1e1d82b75200439a7a97ce9e2a79c282
Jalen-Foreman <78430591+Jalen-Foreman@users.noreply.github.com>
2021-11-14 09:18:25 -0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Jalen F on 11/14/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
