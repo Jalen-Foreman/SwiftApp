8e0639373f9e4bb8940465d916a4b9a6cafdd05e
Jalen-Foreman <78430591+Jalen-Foreman@users.noreply.github.com>
2021-11-18 23:23:12 -0500

    Initial Commit

---
+++ CardGameTest/CardGameTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameTestApp.swift
+//  CardGameTest
+//
+//  Created by Jalen F on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct CardGameTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
